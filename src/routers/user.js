53e1e1dd83b9f2be098537b6b31bc75c06c0e6d8
Matheus Ramos <msilvaramos84@gmail.com>
2022-08-03 15:12:51 -0300

    Adding user update and refactoring delete

--- src/routers/user.js
+++ src/routers/user.js
@@ -46,9 +46,9 @@ router.post('/users/login', (req, res) => {
         }
 
         const token = await generateToken(user.user_uid);
-        delete user.user_uid;
         res.status(200).send({ user, token });
     });
+    client.end;
 });
 
 router.post('/users/logout', auth, async (req, res) => {
@@ -57,6 +57,7 @@ router.post('/users/logout', auth, async (req, res) => {
     }).catch(err => {
         res.status(500).send();
     });
+    client.end;
 });
 
 router.post('/users/logoutAll', auth, (req, res) => {
@@ -67,24 +68,54 @@ router.post('/users/logoutAll', auth, (req, res) => {
         }
         res.status(200).send();
     });
-});
-
-router.get('/users', (req, res) => {
-    client.query(`SELECT * FROM users`, (err, result) => {
-        if (!err) {
-            res.send(result.rows);
-        }
-    });
     client.end;
 });
 
-// change when add authentication
+router.get('/users/me', auth, (req, res) => {
+    res.status(200).send(req.user);
+});
+
+router.patch('/users/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    let user;
+    let values = '';
+
+    await client.query(`SELECT * FROM users WHERE user_uid = '${req.user.user_uid}'`).then(result => {
+        user = result.rows[0];
+    }).catch(err => {
+        return res.status(404).send();
+    });
+
+    if (updates.includes('password')) {
+        req.body.password = await bcrypt.hash(user.password, 8);
+    }
+
+    updates.forEach(update => user[update] = req.body[update]);
+
+    for (let i = 0; i < updates.length; i++) {
+        values = values.concat(`${updates[i]} = '${req.body[updates[i]]}', `);
+    }
+    values = values.substring(0, values.length - 2);
+
+    await client.query(`UPDATE users SET ${values} WHERE user_uid = '${req.user.user_uid}'`).then(result => {
+        res.status(200).send(user);
+    }).catch(err => {
+        res.status(400).send(err);
+    });
+    
+    client.end;
+});
 
 router.delete('/users/me', auth, async (req, res) => {
     const user = req.user;
 
-    await client.query(`DELETE FROM tokens WHERE user_uid = '${user.user_uid}'`);
-    await client.query(`DELETE FROM tasks WHERE user_uid = '${user.user_uid}'`);
     await client.query(`DELETE FROM users WHERE user_uid = '${user.user_uid}'`).then(result => {
         res.status(200).send(user);
     }).catch(err => {
