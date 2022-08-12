8849a58275f1edc7bcc15f9781a4810e2987dfe8
Matheus Ramos <msilvaramos84@gmail.com>
2022-08-12 14:51:54 -0300

    Increasing token expiration time

--- src/middleware/generateToken.js
+++ src/middleware/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (user_uid) => {
-    const token = jwt.sign({ id: user_uid }, process.env.JWT_SECRET, { expiresIn: '10s' });
+    const token = jwt.sign({ id: user_uid }, process.env.JWT_SECRET, { expiresIn: '1000s' });
     return token;
 }
 
