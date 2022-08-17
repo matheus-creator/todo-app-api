779f8498d388e0cc3ba478d4e832189e2ca5a5a4
Matheus Ramos <msilvaramos84@gmail.com>
2022-08-17 15:09:54 -0300

    Refactoring authentication

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,26 +1,21 @@
-const jwt = require('jsonwebtoken');
-const client = require('../connection');
+const jwt = require("jsonwebtoken");
+const client = require("../connection");
 
 const auth = async (req, res, next) => {
     try {
         const token = req.cookies.token;
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user_uid = decoded.id;
-        let user;
 
-        await client.query(`SELECT * FROM users WHERE user_uid = '${user_uid}'`).then(result => {            
-            user = result.rows[0];
-        }).catch(err => {
-            throw new Error();
-        });
-        client.end;
+        const result = await client.query(`SELECT * FROM users WHERE user_uid = '${user_uid}'`);
+        const user = result.rows[0];
 
         req.token = token;
         req.user = user;
         next();
     } catch {
-        res.clearCookie('token');
-        res.status(401).send({ error: 'Please authenticate.' });
+        res.clearCookie("token");
+        res.status(401).send({ error: "Please authenticate." });
     }
 };
 
